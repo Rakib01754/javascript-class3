5ff103693c3e8145cff4607dcbd65b415be66157
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-02 15:20:04 +0600

    first commit

---
+++ odd.js
@@ -0,0 +1,6 @@
+number = 100;
+
+while (number >= 10) {
+    console.log(number);
+    number -= 5;
+}
---
+++ while.js
@@ -0,0 +1,10 @@
+var givenEgg = 0;
+
+while (givenEgg <= 5) {
+    console.log('Amake Dewa hoyni');
+    console.log(givenEgg);
+    givenEgg++;
+
+
+}
+
