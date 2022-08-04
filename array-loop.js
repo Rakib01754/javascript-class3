6dfe57c3f54f6569be50143f615466f6a97abcb0
Rakib01754 <rakibul.freelancer567@gmail.com>
2022-08-04 23:10:24 +0600

    updated

--- array-loop.js
+++ array-loop.js
@@ -1,12 +1,14 @@
 var myPartnaers = ['rana', 'nadim', 'rafi', 'suhel', 'opu', 'opurba'];
 
 for (var i = 0; i < myPartnaers.length; i++) {
+
+
     console.log(myPartnaers[i]);
 }
 
 
-var playersAge = [24, 30, 42, 38, 22, 37];
+// var playersAge = [24, 30, 42, 38, 22, 37];
 
-for (var i = 0; i < playersAge.length; i++) {
-    console.log(playersAge[i]);
-}
+// for (var i = 0; i < playersAge.length; i++) {
+//     console.log(playersAge[i]);
+// }
--- break.js
+++ break.js
@@ -1,12 +1,8 @@
-// var rollNumbers = [15, 20, 64, 30, 56, 78];
-// for (var i = 0; i < rollNumbers.length; i++) {
-//     var roll = rollNumbers[i];
-//     console.log(roll);
-// var i = 0;
-// for (i = 0; i < 5; i++) { };
-// console.log(i);
-
-var i = 5;
-for (; i < 5; i++) {
-    console.log(i);
+var rollNumbers = [15, 20, 64, 30, 56, 78];
+for (var i = 0; i < rollNumbers.length; i++) {
+    var roll = rollNumbers[i];
+    if (roll == 64) {
+        break;
+    }
+    console.log(roll);
 }
